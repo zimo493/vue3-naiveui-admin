b645828d7aebc273b588aa45b3a22b01b5d39331
zimo493 <2081182432@qq.com>
2025-04-09 15:53:37 +0800

    feat: :sparkles: 新增图标工具和类型判断工具
    添加 icon.ts 文件，实现图标渲染和创建功能
    
    BREAKING CHANGE: 添加 is.ts 文件，提供多种类型判断方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,5 +4,7 @@ export * from "./comm";
 export * from "./jsencrypt";
 export * from "./storage";
 export * from "./request";
+export * from "./icon";
+export * from "./is";
 
 export default request;
