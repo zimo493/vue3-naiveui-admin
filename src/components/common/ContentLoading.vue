c61134e1685ed4b05dc0ee087aafa3889ae71261
zimo493 <2081182432@qq.com>
2025-04-10 16:06:54 +0800

    feat: :sparkles: 新增内容加载组件和路由参数示例页面
    - 添加 ContentLoading 组件，用于显示加载中的状态
    
    BREAKING CHANGE: - 创建 route-param.vue 页面，演示如何获取路由参数

---
+++ src/components/common/ContentLoading.vue
@@ -0,0 +1,11 @@
+<template>
+  <n-flex vertical flex-1 align="center" justify="center">
+    <n-text type="primary">
+      <nova-icon :size="48" icon="line-md:loading-twotone-loop" />
+    </n-text>
+  </n-flex>
+</template>
+
+<script lang="ts">
+export default { name: "ContentLoading" };
+</script>
---
+++ src/views/demo/route-param.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-5">路由参数type：{{ query }}</div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "Other",
+  inheritAttrs: false,
+});
+import { useRoute } from "vue-router";
+
+// 获取query参数
+const query = useRoute().query.type as string;
+</script>
