ff28aa41df38fb4e7eaf6b25c4e0d77390d2c2e8
zimo493 <2081182432@qq.com>
2025-04-11 15:52:20 +0800

    refactor: :recycle: 重构 CommonStatus 组件
    将模板移至 script 标签内，使用 tsx 语法

--- src/components/common/CommonStatus.vue
+++ src/components/common/CommonStatus.vue
@@ -1,24 +1,19 @@
-<template>
-  <n-tag :bordered="bordered" :type="value === 1 ? 'success' : 'error'">
-    {{ value === 1 ? "正常" : "禁用" }}
-  </n-tag>
-</template>
-<script lang="ts">
-export default { name: "CommonStatus" };
-</script>
-<script lang="ts" setup>
-defineProps({
-  value: {
-    required: true,
-    type: Number,
+<script lang="tsx">
+import { NTag } from "naive-ui";
+
+export default defineComponent({
+  name: "CommonStatus",
+  props: {
+    value: { type: Number, required: true },
+    round: { type: Boolean, default: false },
+    bordered: { type: Boolean, default: false },
   },
-  round: {
-    type: Boolean,
-    default: false,
-  },
-  bordered: {
-    type: Boolean,
-    default: false,
+  setup({ value, round, bordered }) {
+    return () => (
+      <NTag bordered={bordered} round={round} type={value === 1 ? "success" : "error"}>
+        {value === 1 ? "正常" : "禁用"}
+      </NTag>
+    );
   },
 });
 </script>
