fef186f9dc2c3a465fea240a5dff8e6a4690439f
zimo493 <2081182432@qq.com>
2025-04-11 15:56:24 +0800

    feat: :sparkles: 基于 FormPro 封装 DialogForm 和 DrawerForm 组件
    - 这些组件用于创建带有表单的对话框和抽屉
    
    BREAKING CHANGE: - 支持自定义表单配置、宽度、位置等属性

---
+++ src/components/custom/DialogForm/index.vue
@@ -0,0 +1,135 @@
+<template>
+  <n-modal
+    v-model:show="modal.visible"
+    preset="card"
+    :title="modal.title"
+    :on-mask-click="cancel"
+    :style="{ width: width + 'px' }"
+    :segmented="{ content: true, action: true }"
+    @close="cancel"
+  >
+    <slot name="header" />
+    <n-spin :show="show">
+      <FormPro
+        ref="formProRef"
+        v-bind="formConfig"
+        :modelValue="val"
+        :is-look="isLook"
+        useType="submit"
+        @submit="submit"
+      >
+        <template v-for="item in Object.keys($slots)" #[item]>
+          <slot :name="item" />
+        </template>
+      </FormPro>
+    </n-spin>
+
+    <slot name="footer" />
+    <template #action>
+      <n-space justify="end">
+        <n-button v-if="!isLook" type="primary" @click="handleSubmit">
+          <template #icon>
+            <Icones icon="ant-design:check-outlined" />
+          </template>
+          提交
+        </n-button>
+        <n-button strong secondary @click="cancel">
+          <template #icon>
+            <Icones icon="ant-design:close-outlined" />
+          </template>
+          取消
+        </n-button>
+      </n-space>
+    </template>
+  </n-modal>
+</template>
+<script lang="ts">
+export default { name: "DialogForm" };
+</script>
+<script setup lang="ts">
+import { DrawerPlacement } from "naive-ui";
+import { FormOption } from "@/components/custom/FormPro/types";
+import FormPro from "@/components/custom/FormPro/index.vue";
+
+const props = defineProps({
+  formConfig: {
+    required: true,
+    type: Object as PropType<FormOption<Recordable>>,
+  },
+  modelValue: {
+    required: true,
+    type: Object as PropType<Recordable>,
+  },
+  placement: {
+    type: String as PropType<DrawerPlacement>,
+    default: "right",
+  },
+  width: {
+    type: Number,
+    default: 700,
+  },
+  isLook: {
+    type: Boolean,
+    default: false,
+  },
+});
+const modal = ref<{
+  title: string;
+  visible: boolean;
+}>({
+  title: "",
+  visible: false,
+});
+
+const val = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(v) {
+    emit("update:modelValue", v);
+  },
+});
+const emit = defineEmits<{
+  (e: "update:modelValue", v: Recordable): void;
+  (e: "submit", v: Recordable): void;
+  (e: "cancel"): void;
+}>();
+
+const show = ref<boolean>(false); // 加载状态
+const formProRef = ref<InstanceType<typeof FormPro>>();
+
+// 点击提交
+const handleSubmit = () => formProRef.value?.submit();
+
+/** 提交 */
+const submit = (val: Recordable) => emit("submit", val);
+
+/** 取消 */
+const cancel = () => {
+  formProRef.value?.reset();
+  modal.value = { title: "", visible: false };
+  emit("cancel");
+};
+
+defineExpose({
+  /**
+   * 打开弹窗
+   * @param title 标题
+   * @param data 弹窗数据
+   */
+  open: (title: string, data: Recordable) => {
+    console.log();
+
+    modal.value = { title, visible: true };
+    val.value = unref(data);
+  },
+  close: () => {
+    formProRef.value?.reset();
+    modal.value = { title: "", visible: false };
+  },
+  // 开启加载
+  startLoading: () => (show.value = true),
+  // 关闭加载
+  hideLoading: () => (show.value = false),
+});
+</script>
