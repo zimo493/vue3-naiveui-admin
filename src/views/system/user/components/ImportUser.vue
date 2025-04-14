0e068ef2bdde16bcc09c2a96112ce8bf31f79233
zimo493 <2081182432@qq.com>
2025-04-14 11:39:43 +0800

    feat: :sparkles: 新增用户导入功能并优化用户管理页面
    - 添加用户导入组件，支持 .xls 和 .xlsx 文件导入
    
    BREAKING CHANGE: - 实现用户新增、编辑功能，使用抽屉表单

---
+++ src/views/system/user/components/ImportUser.vue
@@ -0,0 +1,189 @@
+<template>
+  <n-modal
+    v-model:show="visible"
+    w-500px
+    preset="card"
+    title="用户导入"
+    :on-mask-click="cancel"
+    :segmented="{ content: true, action: true }"
+    @close="cancel"
+  >
+    <n-alert v-if="error" class="mb-[10px]" :title="error" type="error">
+      <n-table :bordered="false" :single-line="false">
+        <n-data-table
+          :columns="[
+            { title: '序号', key: 'no' },
+            { title: '错误信息', key: 'title' },
+          ]"
+          :data="resultData"
+          :bordered="false"
+          size="small"
+        />
+      </n-table>
+    </n-alert>
+    <n-upload
+      ref="uploadRef"
+      multiple
+      directory-dnd
+      accept=".xls,.xlsx"
+      :max="1"
+      :default-upload="false"
+      :custom-request="customRequest"
+      :on-error="handleError"
+      :on-remove="handleRemove"
+      @before-upload="beforeUpload"
+    >
+      <n-upload-dragger>
+        <div style="margin-bottom: 12px">
+          <Icones icon="ep:upload-filled" :size="56" />
+        </div>
+        <n-text style="font-size: 16px">点击或者拖动文件到该区域来上传</n-text>
+        <n-p depth="3" style="margin: 8px 0 0">请不要上传敏感数据，以免造成不必要的损失。</n-p>
+      </n-upload-dragger>
+    </n-upload>
+    <n-flex vertical align="center">
+      <div>
+        <span>
+          仅允许导入
+          <n-text type="error">.xlsx</n-text>
+          /
+          <n-text type="error">.xls</n-text>
+          格式文件。
+        </span>
+        <n-button text type="primary" @click="importTemplate">下载模板</n-button>
+      </div>
+      <!-- <div v-html="error"></div> -->
+    </n-flex>
+    <template #action>
+      <n-space justify="end">
+        <n-button type="primary" :loading="loading" @click="handleSubmit">
+          <template #icon>
+            <Icones icon="ant-design:check-outlined" />
+          </template>
+          提交
+        </n-button>
+        <n-button strong secondary @click="cancel">
+          <template #icon>
+            <Icones icon="ant-design:close-outlined" />
+          </template>
+          取消
+        </n-button>
+      </n-space>
+    </template>
+  </n-modal>
+</template>
+<script lang="ts">
+export default { name: "ImportUser" };
+</script>
+<script lang="ts" setup>
+import { type UploadFileInfo, type UploadCustomRequestOptions, type UploadInst } from "naive-ui";
+import { useLoading } from "@/hooks";
+import UserAPI from "@/api/system/user";
+import { MIMETYPE, ResultEnum } from "@/enums";
+import { exportFile } from "@/utils";
+
+const { loading, startLoading, endLoading } = useLoading();
+
+defineExpose({
+  open: () => {
+    visible.value = true;
+  },
+});
+
+const emit = defineEmits<{
+  (e: "success"): void;
+}>();
+
+const uploadRef = ref<UploadInst | null>(null);
+
+const visible = ref(false);
+
+const resultData = ref<{ no: number; title: string }[]>([]);
+
+/** 下载模板操作 */
+const importTemplate = () => {
+  UserAPI.downloadTemplate().then((response) => {
+    window.$message.loading("正在下载数据，请稍候...");
+    const fileName = decodeURI(response.headers["content-disposition"].split(";")[1].split("=")[1]);
+
+    exportFile(response.data, MIMETYPE.xlsx, fileName);
+  });
+};
+
+/** 文件上传前回调 */
+const beforeUpload = async (data: { file: UploadFileInfo; fileList: UploadFileInfo[] }) => {
+  // 文件类型判断
+  if (!data.file.name.endsWith(".xls") && !data.file.name.endsWith(".xlsx")) {
+    window.$message.error("文件格式不正确，请上传格式为 .xlsx 或 .xls 的文件");
+
+    return false;
+  }
+
+  return true;
+};
+const error = ref("");
+/** 自定义上传 */
+const customRequest = ({
+  file,
+  data,
+  headers,
+  withCredentials,
+  action,
+  onFinish,
+  onError,
+  onProgress,
+}: UploadCustomRequestOptions) => {
+  startLoading();
+  console.log(file);
+  UserAPI.import("1", file.file as File, (e) => {
+    onProgress({
+      percent: Number(((e.loaded / Number(e.total)) * 100).toFixed(2)),
+    });
+  })
+    .then((result) => {
+      console.log(result);
+      onFinish();
+      if (result.code === ResultEnum.SUCCESS && result.invalidCount === 0) {
+        window.$message.success("导入成功，导入数据：" + result.validCount + "条");
+        emit("success");
+        cancel();
+      } else {
+        error.value = `导入结果：${result.invalidCount}条无效数据，${result.validCount}条有效数据`;
+        resultData.value = result.messageList.map((item, idx) => ({
+          no: idx + 1,
+          title: item,
+        }));
+      }
+    })
+    .catch((err) => {
+      onError();
+      console.log(err);
+      error.value = err.toString().replaceAll("Error:", "");
+    })
+    .finally(() => endLoading());
+};
+
+/** 文件移除回调 */
+const handleRemove = () => {
+  error.value = "";
+  resultData.value = [];
+
+  return true;
+};
+
+/** 文件上传失败回调 */
+const handleError = (options: { file: UploadFileInfo; event?: ProgressEvent }) => {
+  console.log(options);
+};
+
+/*** 提交 */
+const handleSubmit = () => {
+  uploadRef.value?.submit();
+};
+/** 取消 */
+const cancel = () => {
+  error.value = "";
+  resultData.value = [];
+  visible.value = false;
+};
+</script>
--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -38,7 +38,7 @@
           @reset="handleReset"
         >
           <template #controls>
-            <n-button v-has-perm="['sys:user:add']" type="primary" @click="handleAdd()">
+            <n-button v-has-perm="['sys:user:add']" type="primary" @click="openDrawer()">
               <template #icon>
                 <icon-park-outline-plus />
               </template>
@@ -48,7 +48,7 @@
               v-has-perm="['sys:user:delete']"
               type="error"
               :disabled="!selectedRowKeys.length"
-              @click="handleBatchDelete()"
+              @click="handleDelete()"
             >
               <template #icon>
                 <icon-park-outline-delete-themes />
@@ -70,6 +70,36 @@
         </SearchTable>
       </n-gi>
     </n-grid>
+
+    <!-- 新增、编辑 -->
+    <DrawerForm
+      ref="drawerFormRef"
+      :form-config="editConfig"
+      :model-value="modelValue"
+      :width="580"
+      @submit="submitForm"
+    >
+      <template #deptId>
+        <n-tree-select
+          v-model:value="modelValue.deptId"
+          :options="deptOptions"
+          key-field="value"
+          label-field="label"
+          placeholder="请选择部门"
+        />
+      </template>
+      <template #roleIds>
+        <n-select
+          v-model:value="modelValue.roleIds"
+          :options="roleOptions"
+          multiple
+          placeholder="请选择角色"
+        />
+      </template>
+    </DrawerForm>
+
+    <!-- 用户导入 -->
+    <ImportUser ref="importUserRef" @success="handleQuery" />
   </div>
 </template>
 <script lang="tsx">
@@ -85,28 +115,38 @@ import {
   NImage,
   NSpace,
   NText,
+  NInput,
 } from "naive-ui";
 import { FormItemType, type FormOption } from "@/components/custom/FormPro/types";
-import { useDict, useLoading } from "@/hooks";
+import { useCompRef, useDict, useLoading } from "@/hooks";
+
+import { DrawerFormInst } from "@/types/inst";
+import { exportFile, InquiryBox } from "@/utils";
+import { MIMETYPE } from "@/enums";
 
 import DeptAPI from "@/api/system/dept";
 import UserAPI from "@/api/system/user";
+import RoleAPI from "@/api/system/role";
 
 import Icones from "@/components/common/Icones.vue";
 import DictTag from "@/components/custom/DictTag/index.vue";
 import CommonStatus from "@/components/common/CommonStatus.vue";
+import ImportUser from "./components/ImportUser.vue";
 
 const { loading, startLoading, endLoading } = useLoading();
 const { gender } = useDict("gender");
 
-onMounted(() => {
+onMounted(async () => {
   getDeptTree(); // 获取部门树
   handleQuery(); // 查询数据
+  roleOptions.value = await RoleAPI.getOptions();
 });
 
 const show = ref<boolean>(false); // 部门加载状态
-const deptOptions = ref<OptionType[]>([]); // 部门选择树
+const deptOptions = ref<OptionType[]>([]); // 部门下拉数据源
 const pattern = ref<string>(""); // 搜索过滤
+
+const roleOptions = ref<OptionType[]>([]); // 角色下拉数据源
 
 /** 查询部门下拉树结构 */
 const getDeptTree = () => {
@@ -189,6 +229,7 @@ const columns = ref<DataTableColumns<User.VO>>([
     key: "avatar",
     align: "center",
     render: ({ avatar, nickname, gender }) => {
+      avatar = avatar || "xx.png"; // 防止空头像
       const size = 40; // 头像尺寸
       const color = gender === 1 ? "#409eff" : gender === 2 ? "#ff80bf" : "#f0a020"; // 背景颜色
 
@@ -249,7 +290,7 @@ const columns = ref<DataTableColumns<User.VO>>([
           text
           type="primary"
           v-slots={{ icon: () => <Icones icon="ant-design:edit-outlined" /> }}
-          onClick={() => handleEdit(row)}
+          onClick={() => openDrawer(row)}
         >
           编辑
         </NButton>
@@ -267,7 +308,7 @@ const columns = ref<DataTableColumns<User.VO>>([
           text
           type="error"
           v-slots={{ icon: () => <Icones icon="ant-design:delete-outlined" /> }}
-          onClick={() => handleDelete(row)}
+          onClick={() => handleDelete(row.id)}
         >
           删除
         </NButton>
@@ -276,32 +317,203 @@ const columns = ref<DataTableColumns<User.VO>>([
   },
 ]);
 
+const editConfig = ref<FormOption<User.Form>>({
+  fields: [
+    {
+      field: "username",
+      label: "用户名",
+      type: FormItemType.Input,
+    },
+    {
+      field: "nickname",
+      label: "用户昵称",
+      type: FormItemType.Input,
+    },
+    {
+      field: "deptId",
+      label: "所属部门",
+      slotName: "deptId",
+    },
+    {
+      field: "gender",
+      label: "性别",
+      type: FormItemType.Select,
+      dict: "gender",
+    },
+    {
+      field: "roleIds",
+      label: "角色",
+      slotName: "roleIds",
+    },
+    {
+      field: "mobile",
+      label: "手机号码",
+      type: FormItemType.Input,
+    },
+    {
+      field: "email",
+      label: "邮箱",
+      type: FormItemType.Input,
+    },
+    {
+      field: "status",
+      label: "状态",
+      type: FormItemType.Select,
+      options: [
+        { label: "正常", value: 1 },
+        { label: "禁用", value: 0 },
+      ],
+    },
+  ],
+  labelWidth: 100,
+  rules: {
+    username: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
+    nickname: [{ required: true, message: "用户昵称不能为空", trigger: "blur" }],
+    deptId: [{ required: true, message: "所属部门不能为空", trigger: "blur" }],
+    roleIds: [{ required: true, type: "array", message: "用户角色不能为空", trigger: "blur" }],
+    email: [
+      {
+        pattern: /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/,
+        message: "请输入正确的邮箱地址",
+        trigger: "blur",
+      },
+    ],
+    mobile: [
+      {
+        pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
+        message: "请输入正确的手机号码",
+        trigger: "blur",
+      },
+    ],
+  },
+});
+/** 初始化表单 */
+const modelValue = ref<User.Form>({});
+/** 新增、编辑 */
+const drawerFormRef = ref<DrawerFormInst>();
+const openDrawer = (row?: User.VO) => {
+  drawerFormRef.value?.open(row ? "编辑用户" : "新增用户", modelValue.value);
+
+  if (row) {
+    drawerFormRef.value?.startLoading();
+    UserAPI.getFormData(row.id)
+      .then((data) => {
+        modelValue.value = {
+          ...data,
+        };
+      })
+      .finally(() => drawerFormRef.value?.hideLoading());
+  }
+};
+/** 表单提交 */
+const submitForm = async (val: User.Form) => {
+  console.log(val, "表单提交");
+
+  val.id ? await UserAPI.update(val.id, val) : await UserAPI.create(val);
+
+  window.$message.success("操作成功");
+
+  drawerFormRef.value?.close();
+  handleQuery();
+};
+
+// 重置密码
+const newPassword = ref<{
+  pwd: string;
+  status: "success" | "warning" | "error";
+  msg: string;
+}>({
+  pwd: "",
+  status: "success",
+  msg: "",
+});
+const handleResetPassword = (row: User.VO) => {
+  window.$dialog.info({
+    title: "系统提示",
+    positiveText: "确定",
+    negativeText: "取消",
+    maskClosable: false,
+    content: () => (
+      <NSpace vertical>
+        <NText>请输入 "{row.username}" 的新密码</NText>
+        <NInput
+          v-model:value={newPassword.value.pwd}
+          type={"password"}
+          show-password-on={"mousedown"}
+          placeholder="请输入密码"
+          status={newPassword.value.status}
+        />
+        {newPassword.value.status !== "success" && (
+          <NText type={newPassword.value.status}>{newPassword.value.msg}</NText>
+        )}
+      </NSpace>
+    ),
+    onPositiveClick: () => {
+      return new Promise<void>((resolve) => {
+        // 校验密码
+        const { pwd } = newPassword.value;
+
+        if (pwd.length < 6 || pwd.length > 20) {
+          newPassword.value.status = "warning";
+          newPassword.value.msg = "密码长度必须介于 6 和 20 之间";
+        } else if (/[<>"'|\\]/.test(pwd)) {
+          newPassword.value.status = "error";
+          newPassword.value.msg = "不能包含非法字符";
+        } else {
+          newPassword.value.status = "success";
+          newPassword.value.msg = "";
+          const { pwd } = newPassword.value;
+
+          UserAPI.resetPassword(row.id, pwd)
+            .then(() => window.$message.success("密码重置成功，请牢记新密码：" + pwd))
+            .then(() => resolve());
+        }
+      });
+    },
+    onAfterLeave: () => {
+      newPassword.value = {
+        pwd: "",
+        status: "success",
+        msg: "",
+      };
+    },
+  });
+};
+
 /** 选中行 */
 const selectedRowKeys = ref<string[]>([]);
 const handleCheck = (keys: DataTableRowKey[]) => (selectedRowKeys.value = keys as string[]);
+/**
+ * 删除用户
+ *
+ * @param id  用户ID
+ */
+const handleDelete = (id?: string) => {
+  const userIds = [id || selectedRowKeys.value].join(",");
 
-// 新增
-const handleAdd = () => {};
-
-// 编辑
-const handleEdit = (row: User.VO) => {};
-
-// 重置密码
-const handleResetPassword = (row: User.VO) => {};
-
-// 删除
-const handleDelete = (row: User.VO) => {};
-
-// 批量删除
-const handleBatchDelete = () => {
-  console.log(selectedRowKeys.value);
+  InquiryBox(`确定要删除选中的用户吗？`).then(() => {
+    UserAPI.deleteByIds(userIds).then(() => {
+      window.$message.success("删除成功");
+      handleQuery();
+    });
+  });
 };
 
 // 导入
-const handleImport = () => {};
+const importUserRef = useCompRef(ImportUser);
+const handleImport = () => {
+  importUserRef.value?.open();
+};
 
 // 导出
-const handleExport = () => {};
+const handleExport = () => {
+  UserAPI.export(queryParams.value).then((response) => {
+    window.$message.loading("正在下载数据，请稍候...");
+    const fileName = decodeURI(response.headers["content-disposition"].split(";")[1].split("=")[1]);
+
+    exportFile(response.data, MIMETYPE.xlsx, fileName);
+  });
+};
 
 /** 重置 */
 const handleReset = () => {
