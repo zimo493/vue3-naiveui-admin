5d5575f41629b93cc24bbc649868bd25ddbad181
zimo493 <2081182432@qq.com>
2025-05-08 10:08:46 +0800

    build: :package: (unocss)更新 UnoCSS 配置以增强功能
    - 引入 transformerDirectives 转换器以支持指令

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig, presetAttributify, presetUno, transformerVariantGroup } from "unocss";
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
 
 // https://github.com/unocss/unocss
 export default defineConfig({
-  presets: [presetUno({ dark: "class" }), presetAttributify()],
+  presets: [
+    presetUno(), // UnoCSS 的基础预设
+    presetAttributify(), // 属性化模式预设，允许你通过属性来使用类名
+    presetIcons(), // 图标预设
+  ],
+  // 自定义快捷类
   shortcuts: {
     "wh-full": "w-full h-full",
     "flex-center": "flex justify-center items-center",
@@ -10,5 +22,8 @@ export default defineConfig({
     "flex-x-center": "flex justify-center",
     "flex-y-center": "flex items-center",
   },
-  transformers: [transformerVariantGroup()],
+  transformers: [
+    transformerDirectives(), // 启用指令转换器，允许动态绑定 UnoCSS 类
+    transformerVariantGroup(), // 启用变体分组转换器，允许对样式变体进行分组。
+  ],
 });
