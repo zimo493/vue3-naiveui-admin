010ac205bc99cdb5f8f88da05c905b72846d640e
zimo493 <2081182432@qq.com>
2025-05-12 09:41:14 +0800

    style: :lipstick: (components)修改全局加载组件的type为`info`类型

--- src/components/common/ContentLoading.vue
+++ src/components/common/ContentLoading.vue
@@ -1,6 +1,6 @@
 <template>
   <n-flex vertical flex-1 align="center" justify="center">
-    <n-text type="primary">
+    <n-text type="info">
       <Icones :size="48" icon="line-md:loading-twotone-loop" />
     </n-text>
   </n-flex>
