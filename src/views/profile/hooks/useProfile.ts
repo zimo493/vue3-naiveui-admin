b2f5dd700fb1d8787fedf085348cdff9d7510dcc
zimo493 <2081182432@qq.com>
2025-05-28 17:24:53 +0800

    feat: :sparkles: (profile)重构个人资料页面并添加新功能
    - 新增 useProfile 钩子以封装个人资料相关逻辑
    
    BREAKING CHANGE: - 添加修改头像、修改基本信息、修改密码、绑定手机和邮箱等功能，增加用户信息和安全设置组件

---
+++ src/views/profile/hooks/useProfile.ts
@@ -0,0 +1,311 @@
+import { type FormItemRule } from "naive-ui";
+import { type FormOption, FormItemType } from "@/components/custom/FormPro/types";
+
+import { useAuthStoreHook } from "@/store";
+import { useCompRef, useDict, useLoading } from "@/hooks";
+import { spin, startSpin, endSpin } from "@/utils";
+
+import FileAPI from "@/api/file";
+import UserAPI from "@/api/system/user";
+
+import ImageCut from "@/components/custom/ImageCut.vue";
+import DialogForm from "@/components/custom/DialogForm.vue";
+
+export const useProfile = () => {
+  const authStore = useAuthStoreHook();
+  const { gender } = useDict("gender");
+  const { loading, startLoading, endLoading } = useLoading();
+
+  onMounted(async () => {
+    await loadUserProfile();
+  });
+
+  /** 用户信息 */
+  const userProfile = ref<User.ProfileVO>({});
+
+  /** 性别图标 */
+  const genderIcon = computed(() => {
+    if (userProfile.value.gender === 1) {
+      return "ant-design:man-outlined";
+    } else if (userProfile.value.gender === 2) {
+      return "ant-design:woman-outlined";
+    } else {
+      return "ant-design:question-outlined";
+    }
+  });
+  const genderTagType = computed(() => {
+    const type = gender.value.find((item) => +item.value === userProfile.value.gender)?.tagType;
+
+    if (type === "danger") return "error";
+
+    return type ?? "default";
+  });
+
+  /** 加载用户信息 */
+  const loadUserProfile = async () => {
+    startLoading();
+    try {
+      userProfile.value = await UserAPI.getProfile();
+    } finally {
+      endLoading();
+    }
+  };
+
+  /** 修改头像 */
+  const isEdit = ref(false); // 是否显示修改头像的图标
+  const imageCutRef = useCompRef(ImageCut);
+  const changeAvatar = () => imageCutRef.value?.open();
+  /**
+   * 上传并设置头像
+   * @param file 文件
+   */
+  const handleUploadSetAvatar = async (file: File) => {
+    // 调用文件上传API
+    try {
+      imageCutRef.value?.startLoading();
+      const { url: avatar } = await FileAPI.uploadFile(file);
+
+      // 更新用户头像
+      authStore.userInfo.avatar = avatar;
+      userProfile.value.avatar = avatar;
+      // 更新用户信息
+      await UserAPI.updateProfile({
+        avatar,
+      });
+      imageCutRef.value?.close();
+      window.$message.success("头像更新成功！");
+    } catch (error) {
+      console.error("头像上传失败：" + error);
+      window.$message.error("头像上传失败！");
+    } finally {
+      imageCutRef.value?.endLoading();
+    }
+  };
+
+  /** 修改用户信息配置 */
+  const userProfileForm = ref<User.ProfileForm>({}); // 用户信息表单
+  const userProfileFormRef = useCompRef(DialogForm);
+  const userProfileFormConfig = ref<FormOption<User.ProfileForm>>({
+    fields: [
+      { field: "nickname", label: "昵称", type: FormItemType.Input },
+      { field: "gender", label: "性别", type: FormItemType.Select, dict: "gender" },
+    ],
+    rules: {
+      nickname: [{ required: true, message: "请输入昵称", trigger: "blur" }],
+      gender: [{ required: true, type: "number", message: "请选择性别", trigger: "change" }],
+    },
+    labelWidth: 80,
+  });
+  /** 修改基本信息 */
+  const updateUserProfile = () => {
+    userProfileForm.value = {
+      id: userProfile.value.id,
+      gender: userProfile.value.gender,
+      nickname: userProfile.value.nickname,
+    };
+    userProfileFormRef.value?.open("账号信息修改", userProfileForm.value);
+  };
+
+  /** 修改基本信息提交 */
+  const submitUserProfile = async (val: User.ProfileForm) => {
+    console.log(val, "提交");
+    try {
+      startSpin();
+      await UserAPI.updateProfile(val);
+      window.$message.success("账号资料修改成功");
+      await loadUserProfile();
+      userProfileFormRef.value?.close();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      endSpin();
+    }
+  };
+  /** 修改用户密码配置 */
+  const passwordChangeForm = ref<User.PasswordChangeForm>({}); // 修改密码表单
+  const passwordChangeFormRef = useCompRef(DialogForm);
+  const passwordChangeFormConfig = ref<FormOption<User.PasswordChangeForm>>({
+    fields: [
+      { field: "oldPassword", label: "旧密码", type: FormItemType.Password },
+      { field: "newPassword", label: "新密码", type: FormItemType.Password },
+      { field: "confirmPassword", label: "确认密码", type: FormItemType.Password },
+    ],
+    rules: {
+      oldPassword: [{ required: true, message: "请输入旧密码", trigger: "blur" }],
+      newPassword: [{ required: true, message: "请输入新密码", trigger: "blur" }],
+      confirmPassword: [
+        { required: true, message: "请输入确认密码", trigger: "blur" },
+        {
+          validator: (rule: FormItemRule, value: string) =>
+            new Promise((resolve, reject) =>
+              value !== passwordChangeForm.value.newPassword
+                ? reject(new Error("两次输入的密码不一致"))
+                : resolve()
+            ),
+          trigger: "blur",
+        },
+      ],
+    },
+    labelWidth: 80,
+  });
+
+  /** 修改用户密码 */
+  const updatePassword = () =>
+    passwordChangeFormRef.value?.open("修改密码", passwordChangeForm.value);
+  /** 修改用户密码提交 */
+  const submitPassword = async (val: User.PasswordChangeForm) => {
+    console.log(val, "提交");
+    try {
+      startSpin();
+      await UserAPI.changePassword(val);
+      window.$message.success("密码修改成功");
+      passwordChangeFormRef.value?.close();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      endSpin();
+    }
+  };
+
+  /** 修改手机信息配置 */
+  const mobileUpdateForm = ref<User.MobileUpdateForm>({}); // 修改手机表单
+  const mobileUpdateFormRef = useCompRef(DialogForm);
+  const mobileUpdateFormConfig = ref<FormOption<User.MobileUpdateForm>>({
+    fields: [
+      { field: "mobile", label: "手机号", type: FormItemType.Input },
+      { field: "code", label: "验证码", slotName: "code" },
+    ],
+    rules: {
+      mobile: [
+        { required: true, message: "请输入手机号", trigger: "blur" },
+        { pattern: /^1[3-9]\d{9}$/, message: "请输入正确的手机号", trigger: "blur" },
+      ],
+      code: [{ required: true, message: "请输入验证码", trigger: "blur" }],
+    },
+    labelWidth: 80,
+  });
+  /** 绑定手机 */
+  const updateMobile = () => {
+    mobileUpdateForm.value = {
+      mobile: userProfile.value.mobile,
+    };
+    mobileUpdateFormRef.value?.open("绑定手机", mobileUpdateForm.value);
+  };
+
+  /** 绑定手机获取验证码 */
+  const sendMobileCode = async () => {
+    try {
+      startSpin();
+      await UserAPI.sendMobileCode(mobileUpdateForm.value.mobile);
+      window.$message.success("验证码发送成功");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      endSpin();
+    }
+  };
+
+  /** 绑定手机提交 */
+  const submitMobile = async (val: User.MobileUpdateForm) => {
+    try {
+      startSpin();
+      await UserAPI.bindOrChangeMobile(val);
+      window.$message.success("手机绑定成功");
+      mobileUpdateFormRef.value?.close();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      endSpin();
+    }
+  };
+
+  /** 绑定邮箱信息配置 */
+  const emailUpdateForm = ref<User.EmailUpdateForm>({}); // 修改邮箱表单
+  const emailUpdateFormRef = useCompRef(DialogForm);
+  const emailUpdateFormConfig = ref<FormOption<User.EmailUpdateForm>>({
+    fields: [
+      { field: "email", label: "邮箱", type: FormItemType.Input },
+      { field: "code", label: "验证码", slotName: "code" },
+    ],
+    rules: {
+      email: [
+        { required: true, message: "请输入邮箱", trigger: "blur" },
+        {
+          pattern: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
+          message: "请输入正确的邮箱",
+          trigger: "blur",
+        },
+      ],
+      code: [{ required: true, message: "请输入验证码", trigger: "blur" }],
+    },
+    labelWidth: 80,
+  });
+  /** 绑定邮箱 */
+  const updateEmail = () => {
+    emailUpdateForm.value = {
+      email: userProfile.value.email,
+    };
+    emailUpdateFormRef.value?.open("绑定邮箱", emailUpdateForm.value);
+  };
+
+  /** 绑定邮箱获取验证码 */
+  const sendEmailCode = async () => {
+    try {
+      startSpin();
+      await UserAPI.sendEmailCode(emailUpdateForm.value.email);
+      window.$message.success("验证码发送成功");
+    } catch (err) {
+      console.error(err);
+    } finally {
+      endSpin();
+    }
+  };
+
+  /** 绑定邮箱提交 */
+  const submitEmail = async (val: User.EmailUpdateForm) => {
+    try {
+      startSpin();
+      await UserAPI.bindOrChangeEmail(val);
+      window.$message.success("邮箱绑定成功");
+      emailUpdateFormRef.value?.close();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      endSpin();
+    }
+  };
+
+  return {
+    spin,
+    isEdit,
+    loading,
+    genderIcon,
+    userProfile,
+    genderTagType,
+    imageCutRef,
+    userProfileForm,
+    userProfileFormRef,
+    userProfileFormConfig,
+    passwordChangeForm,
+    passwordChangeFormRef,
+    passwordChangeFormConfig,
+    mobileUpdateForm,
+    mobileUpdateFormRef,
+    mobileUpdateFormConfig,
+    emailUpdateForm,
+    emailUpdateFormRef,
+    emailUpdateFormConfig,
+    changeAvatar,
+    handleUploadSetAvatar,
+    updateUserProfile,
+    submitUserProfile,
+    updatePassword,
+    submitPassword,
+    updateMobile,
+    sendMobileCode,
+    submitMobile,
+    updateEmail,
+    sendEmailCode,
+    submitEmail,
+  };
+};
--- src/views/profile/index.tsx
+++ src/views/profile/index.tsx
@@ -1,36 +1,64 @@
-import { useAuthStoreHook } from "@/store";
-import { useCompRef, useLoading } from "@/hooks";
+import { useProfile } from "./hooks/useProfile";
 
-import FileAPI from "@/api/file";
-import UserAPI from "@/api/system/user";
-
-import { NSpin, NGrid, NGi, NCard, NFlex, NAvatar, NH3, NTag, NEl, NDivider } from "naive-ui";
+import {
+  NSpin,
+  NGrid,
+  NGi,
+  NCard,
+  NFlex,
+  NH3,
+  NDivider,
+  NAvatar,
+  NTag,
+  NEl,
+  NText,
+  NButton,
+  NDescriptions,
+  NDescriptionsItem,
+  NInputGroup,
+  NInput,
+} from "naive-ui";
 import Icones from "@/components/common/Icones.vue";
 import ImageCut from "@/components/custom/ImageCut.vue";
+import CommonWrapper from "@/components/common/CommonWrapper.vue";
+import DialogForm from "@/components/custom/DialogForm.vue";
 
 export default defineComponent({
   name: "Profile",
   setup() {
-    const { loading, startLoading, endLoading } = useLoading();
-
-    const authStore = useAuthStoreHook();
-
-    onMounted(async () => {
-      await loadUserProfile();
-    });
-
-    /** 用户信息 */
-    const userProfile = ref<User.ProfileVO>({});
-
-    /** 加载用户信息 */
-    const loadUserProfile = async () => {
-      startLoading();
-      try {
-        userProfile.value = await UserAPI.getProfile();
-      } finally {
-        endLoading();
-      }
-    };
+    const {
+      spin,
+      isEdit,
+      loading,
+      genderIcon,
+      userProfile,
+      imageCutRef,
+      genderTagType,
+      userProfileForm,
+      userProfileFormRef,
+      userProfileFormConfig,
+      passwordChangeForm,
+      passwordChangeFormRef,
+      passwordChangeFormConfig,
+      mobileUpdateForm,
+      mobileUpdateFormRef,
+      mobileUpdateFormConfig,
+      emailUpdateForm,
+      emailUpdateFormRef,
+      emailUpdateFormConfig,
+      changeAvatar,
+      handleUploadSetAvatar,
+      updateUserProfile,
+      submitUserProfile,
+      updatePassword,
+      submitPassword,
+      updateMobile,
+      sendMobileCode,
+      submitMobile,
+      updateEmail,
+      sendEmailCode,
+      submitEmail,
+    } = useProfile();
 
     /** 错误时显示的头像 */
     const ErrorAvatar = () => (
@@ -41,8 +69,6 @@ export default defineComponent({
 
     /** 头像组件 */
     const Avatar = () => {
-      const isEdit = ref(false); // 是否显示修改头像的图标
-
       return (
         <NFlex
           class="relative inline-block"
@@ -69,49 +95,81 @@ export default defineComponent({
       );
     };
 
-    /** 修改头像 */
-    const imageCutRef = useCompRef(ImageCut);
-    const changeAvatar = () => imageCutRef.value?.open();
-    /**
-     * 上传并设置头像
-     * @param file 文件
-     */
-    const handleSubmit = async (file: File) => {
-      // 调用文件上传API
-      try {
-        imageCutRef.value?.startLoading();
-        const { url: avatar } = await FileAPI.uploadFile(file);
-
-        // 更新用户头像
-        authStore.userInfo.avatar = avatar;
-        userProfile.value.avatar = avatar;
-        // 更新用户信息
-        await UserAPI.updateProfile({
-          avatar,
-        });
-        imageCutRef.value?.close();
-        window.$message.success("头像更新成功！");
-      } catch (error) {
-        console.error("头像上传失败：" + error);
-        window.$message.error("头像上传失败！");
-      } finally {
-        imageCutRef.value?.endLoading();
-      }
-    };
-
-    /** 角色信息组件 */
-    const RoleInfo = () =>
-      userProfile.value.roleNames?.split(",").map((role) => (
-        <NTag key={role} bordered={false} type="primary">
-          {role}
-        </NTag>
-      ));
+    /** 个人信息组件 */
+    const UserInfo = () => (
+      <>
+        <NFlex class="mt-4 mb-3" align="center" justify="center" size={[4, 0]}>
+          <NH3 class="font-bold m-0">{userProfile.value.nickname}</NH3>
+          <CommonWrapper onClick={() => updateUserProfile()}>
+            <Icones icon="ant-design:edit-outlined" />
+          </CommonWrapper>
+        </NFlex>
+        {/* 角色信息 */}
+        <NFlex>
+          {userProfile.value.roleNames?.split(",").map((role) => (
+            <NTag key={role} bordered={false} type="primary">
+              {role}
+            </NTag>
+          ))}
+        </NFlex>
+      </>
+    );
 
     /** 账号信息组件 */
-    // const AccountInfo = () => {};
+    const AccountInfo = () => (
+      <NCard title="账号信息" segmented={{ content: true }}>
+        <NDescriptions label-placement="left" label-style="width: 20%" column={1} bordered>
+          <NDescriptionsItem label="用户名">
+            <NFlex align="center">
+              <NText>{userProfile.value.username}</NText>
+              <NTag bordered={false} type={genderTagType.value}>
+                <Icones icon={genderIcon.value} />
+              </NTag>
+            </NFlex>
+          </NDescriptionsItem>
+          <NDescriptionsItem label="手机号码">
+            <NFlex align="center">
+              <NText>{userProfile.value.mobile}</NText>
+              <NButton type="primary" quaternary size="small" onClick={() => updateMobile()}>
+                更换
+              </NButton>
+            </NFlex>
+          </NDescriptionsItem>
+          <NDescriptionsItem label="邮箱地址">
+            <NFlex align="center">
+              <NText>{userProfile.value.email}</NText>
+              <NButton type="primary" quaternary size="small" onClick={() => updateEmail()}>
+                更换
+              </NButton>
+            </NFlex>
+          </NDescriptionsItem>
+          <NDescriptionsItem label="部门">{userProfile.value.deptName}</NDescriptionsItem>
+          <NDescriptionsItem label="创建时间">{userProfile.value.createTime}</NDescriptionsItem>
+        </NDescriptions>
+      </NCard>
+    );
 
     /** 安全设置组件 */
-    // const SafeSetting = () => {};
+    const SafeSetting = () => (
+      <NCard title="安全设置" segmented={{ content: true }}>
+        <NFlex align="center" justify="space-between" class="p-2">
+          <NFlex vertical>
+            <NText class="text-16px">账户密码</NText>
+            <NText depth={3}>定期修改密码有助于保护账户安全</NText>
+          </NFlex>
+          <NButton
+            quaternary
+            type="primary"
+            v-slots={{
+              icon: () => <Icones icon="ant-design:edit-outlined" />,
+            }}
+            onClick={() => updatePassword()}
+          >
+            修改密码
+          </NButton>
+        </NFlex>
+      </NCard>
+    );
 
     return () => (
       <NSpin show={loading.value}>
@@ -120,27 +178,96 @@ export default defineComponent({
             <NCard>
               <NFlex vertical align="center" class="p-3">
                 {Avatar()}
-                <NH3 class="font-bold">{userProfile.value.nickname}</NH3>
-                <NFlex>{RoleInfo()}</NFlex>
+                {UserInfo()}
               </NFlex>
               <NDivider />
+              1111111
             </NCard>
           </NGi>
           <NGi span={16}>
             <NGrid yGap={10} cols={1}>
-              <NGi>
-                <NCard>1111111</NCard>
-              </NGi>
-              <NGi>
-                <NCard>222222</NCard>
-              </NGi>
+              <NGi>{AccountInfo()}</NGi>
+              <NGi>{SafeSetting()}</NGi>
             </NGrid>
           </NGi>
         </NGrid>
+        {/* 头像裁剪 */}
         <ImageCut
           ref={imageCutRef}
           image={userProfile.value.avatar}
-          onSubmit={(val: File) => handleSubmit(val)}
+          onSubmit={(file: File) => handleUploadSetAvatar(file)}
+        />
+        {/* 修改基本信息 */}
+        <DialogForm
+          width={500}
+          draggable
+          loading={spin.value}
+          ref={userProfileFormRef}
+          form-config={userProfileFormConfig.value}
+          model-value={userProfileForm.value}
+          onSubmit={(val: User.ProfileForm) => submitUserProfile(val)}
+        />
+        {/* 修改密码 */}
+        <DialogForm
+          width={500}
+          draggable
+          loading={spin.value}
+          ref={passwordChangeFormRef}
+          form-config={passwordChangeFormConfig.value}
+          model-value={passwordChangeForm.value}
+          onSubmit={(val: User.PasswordChangeForm) => submitPassword(val)}
+        />
+        {/* 修改绑定手机 */}
+        <DialogForm
+          width={500}
+          draggable
+          loading={spin.value}
+          ref={mobileUpdateFormRef}
+          form-config={mobileUpdateFormConfig.value}
+          model-value={mobileUpdateForm.value}
+          onSubmit={(val: User.MobileUpdateForm) => submitMobile(val)}
+          v-slots={{
+            code: () => (
+              <NInputGroup>
+                <NInput v-model:value={mobileUpdateForm.value.code} placeholder="请输入验证码" />
+                <NButton
+                  type="info"
+                  strong
+                  disabled={!mobileUpdateForm.value.mobile}
+                  loading={spin.value}
+                  onClick={() => sendMobileCode()}
+                >
+                  发送验证码
+                </NButton>
+              </NInputGroup>
+            ),
+          }}
+        />
+        {/* 绑定邮箱 */}
+        <DialogForm
+          width={500}
+          draggable
+          loading={spin.value}
+          ref={emailUpdateFormRef}
+          form-config={emailUpdateFormConfig.value}
+          model-value={emailUpdateForm.value}
+          onSubmit={(val: User.EmailUpdateForm) => submitEmail(val)}
+          v-slots={{
+            code: () => (
+              <NInputGroup>
+                <NInput v-model:value={emailUpdateForm.value.code} placeholder="请输入验证码" />
+                <NButton
+                  type="info"
+                  strong
+                  disabled={!emailUpdateForm.value.email}
+                  loading={spin.value}
+                  onClick={() => sendEmailCode()}
+                >
+                  发送验证码
+                </NButton>
+              </NInputGroup>
+            ),
+          }}
         />
       </NSpin>
     );
