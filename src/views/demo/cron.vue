518376271507fd4074dff600c4c89df028347558
zimo493 <2081182432@qq.com>
2025-06-03 11:39:54 +0800

    feat: :sparkles: (demo)添加定时任务表达式生成功能
    - 新增 Cron 组件，提供定时任务表达式输入和生成功能
    
    - 集成 Crontab 组件用于生成定时任务表达式

---
+++ src/views/demo/cron.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <n-grid x-gap="12" :cols="2">
+      <n-gi>
+        <n-input-group>
+          <n-input v-model:value="cronExpression" placeholder="请输入或生成表达式" clearable />
+          <n-button type="primary" @click="() => crontabRef?.open()">
+            <template #icon>
+              <Icones icon="ant-design:clock-circle-outlined" />
+            </template>
+            生成表达式
+          </n-button>
+        </n-input-group>
+      </n-gi>
+    </n-grid>
+    <!-- 表达式生成 -->
+    <Crontab ref="crontab" v-model="cronExpression" />
+  </div>
+</template>
+<script lang="ts" setup>
+defineOptions({ name: "Cron" });
+
+const crontabRef = useTemplateRef("crontab");
+
+const cronExpression = ref("");
+</script>
