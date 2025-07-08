13cd9770ef8ee97f17baed196a2258d49ff73d1c
zimo493 <2081182432@qq.com>
2025-07-08 13:14:50 +0800

    feat: :sparkles: (components)新增 `ModalForm` 组件
    - 创建 ModalForm.vue 文件，实现了一个可配置的模态表单组件
    
    - 组件支持自定义表单配置、宽度、加载状态等属性
    
    - 提供了打开模态框、关闭模态框和提交表单的功能

---
+++ src/components/custom/ModalForm.vue
@@ -0,0 +1,116 @@
+<template>
+  <n-modal
+    v-model:show="modal.visible"
+    preset="card"
+    :title="modal.title"
+    :on-mask-click="cancel"
+    :style="{ width: `${width}px` }"
+    :segmented="{ content: true, action: true }"
+    @close="cancel"
+  >
+    <slot name="header" />
+    <n-spin :show="loading">
+      <FormProTwo
+        ref="formPro"
+        v-model:model-value="modelValue"
+        :form-config="showFormConfig"
+        :form-props="{ ...(form?.props || {}) }"
+        :grid-props="{ ...(form?.gridProps || {}) }"
+      />
+    </n-spin>
+    <slot name="footer" />
+    <template #action>
+      <n-flex justify="end">
+        <n-button type="primary" :loading="loading" @click="handleSubmit">
+          <template #icon>
+            <Icones icon="ant-design:check-outlined" />
+          </template>
+          提交
+        </n-button>
+        <n-button strong secondary @click="cancel">
+          <template #icon>
+            <Icones icon="ant-design:close-outlined" />
+          </template>
+          取消
+        </n-button>
+      </n-flex>
+    </template>
+  </n-modal>
+</template>
+
+<script lang="ts" setup generic="T extends Recordable">
+interface Props<T> {
+  props?: DialogForm.ModalProps;
+  width?: number;
+  form?: DialogForm.Form;
+  formConfig?: FormPro.FormItemConfig[];
+  loading?: boolean;
+}
+
+defineOptions({ name: "ModalForm" });
+
+const props = withDefaults(defineProps<Props<T>>(), {
+  loading: false,
+  width: 700,
+  scrollHeight: 400,
+});
+
+// 表单参数
+const modelValue = defineModel<T>("modelValue", {
+  default: () => ({}),
+});
+
+const modal = ref<FormModal>({
+  title: "",
+  visible: false,
+});
+
+/**
+ * 展示的表单配置
+ * 优先使用直接传入的 formConfig，如果没有则使用 form.config
+ */
+const showFormConfig = computed(() => {
+  return (props.formConfig || props.form?.config || []).map((item) => ({
+    ...item,
+    span: item.span ?? 24,
+  }));
+});
+
+const emit = defineEmits<{
+  (e: "submit", v: T): void;
+  (e: "cancel"): void;
+}>();
+
+const formProRef = useTemplateRef("formPro");
+// 点击提交
+const handleSubmit = async () => {
+  await formProRef.value?.validate();
+  emit("submit", modelValue.value);
+};
+
+/** 取消 */
+const cancel = () => {
+  formProRef.value?.reset();
+  modal.value = { title: "", visible: false };
+  emit("cancel");
+};
+
+// 暴露表单实例方法
+const dialogFormInstance: DialogForm.Instance = {
+  /**
+   * 打开弹窗
+   * @param title 标题
+   * @param data 表单数据
+   */
+  open: (title: string, data: T) => {
+    modal.value = { title, visible: true };
+    modelValue.value = unref(data);
+  },
+  close: () => {
+    formProRef.value?.reset();
+    modal.value = { title: "", visible: false };
+  },
+};
+
+defineExpose(dialogFormInstance);
+</script>
