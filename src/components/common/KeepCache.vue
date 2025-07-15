2e18b0a5db5e2c61475b3b5f380a1a42d07ea0fd
zimo493 <2081182432@qq.com>
2025-07-15 15:37:08 +0800

    feat: :sparkles: (components)添加 `KeepCache` 组件
    新增 KeepCache 组件，用于路由组件的缓存。
    
    该组件使用 KeepAlive 来缓存指定路由组件。
    
    - 使用 RouterView 和 KeepAlive 实现路由组件缓存
    
    - 动态加载缓存的组件
    
    - 根据路由的 fullPath 设置组件的 key

---
+++ src/components/common/KeepCache.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterView v-slot="{ Component, route }">
+    <KeepAlive :include="routeStore.cacheRoutes">
+      <component :is="Component" :key="route.fullPath" />
+    </KeepAlive>
+  </RouterView>
+</template>
+<script lang="ts" setup>
+import { useRouteStoreHook } from "@/store";
+
+defineOptions({ name: "KeepCache" });
+
+const routeStore = useRouteStoreHook();
+</script>
