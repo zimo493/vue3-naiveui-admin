e6535bff80117da69dc850941cfefebf39211f03
zimo493 <2081182432@qq.com>
2025-07-21 16:18:12 +0800

    style: :lipstick: (theme)更新默认主题颜色
    - 修改信息色为 #3d8bf2
    
    - 修改成功色为 #1fb56f
    
    - 修改警告色为 #ffb020
    
    - 修改错误色为 #f54e4e
    
    - 相应调整各颜色的 hover、pressed 和 suppl 值

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -3,13 +3,13 @@ import type { GlobalThemeOverrides } from "naive-ui";
 /** 默认主色 */
 export const primaryColor = "#ac29e1";
 /** 默认信息色 */
-export const infoColor = "#70c0e8";
+export const infoColor = "#3d8bf2";
 /** 默认成功色 */
-export const successColor = "#18a058";
+export const successColor = "#1fb56f";
 /** 默认警告色 */
-export const warningColor = "#f0a020";
+export const warningColor = "#ffb020";
 /** 默认错误色 */
-export const errorColor = "#d03050";
+export const errorColor = "#f54e4e";
 
 /** 默认主题配置 */
 const themeConfig: GlobalThemeOverrides = {
@@ -19,21 +19,21 @@ const themeConfig: GlobalThemeOverrides = {
     primaryColorPressed: "#9b1dce",
     primaryColorSuppl: "#8e1abd",
     infoColor,
-    infoColorHover: "#8accec",
-    infoColorPressed: "#56b4e4",
-    infoColorSuppl: "#44ade1",
+    infoColorHover: "#5a9cf4",
+    infoColorPressed: "#207af0",
+    infoColorSuppl: "#106fec",
     successColor,
-    successColorHover: "#1cbb67",
-    successColorPressed: "#148549",
-    successColorSuppl: "#117440",
+    successColorHover: "#23cf7f",
+    successColorPressed: "#1b9b5f",
+    successColorSuppl: "#188954",
     warningColor,
-    warningColorHover: "#f2ac3d",
-    warningColorPressed: "#e2910f",
-    warningColorSuppl: "#cf850e",
+    warningColorHover: "#ffbb3f",
+    warningColorPressed: "#ffa501",
+    warningColorSuppl: "#ec9800",
     errorColor,
-    errorColorHover: "#d64965",
-    errorColorPressed: "#b82a46",
-    errorColorSuppl: "#a72640",
+    errorColorHover: "#f76b6b",
+    errorColorPressed: "#f33131",
+    errorColorSuppl: "#f21e1e",
   },
 };
 
