a110247ac3ffbf51b7df4a81c55981cc152ebb51
zimo493 <2081182432@qq.com>
2025-08-01 11:21:46 +0800

    style: :lipstick: (system)优化用户管理密码重置功能的对话框样式
    - 移除了正向按钮的 secondary 和 strong 属性
    
    - 移除了负向按钮的 text 属性

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -460,12 +460,9 @@ const handleResetPassword = (row: User.VO) => {
     positiveText: t("button.ok"),
     negativeText: t("button.cancel"),
     positiveButtonProps: {
-      secondary: true,
-      strong: true,
       renderIcon: () => h(Icones, { icon: "ant-design:check-outlined", size: 16 }),
     },
     negativeButtonProps: {
-      text: true,
       renderIcon: () => h(Icones, { icon: "ant-design:close-outlined", size: 16 }),
     },
     content: () => (
