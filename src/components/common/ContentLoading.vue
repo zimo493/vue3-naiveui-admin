6d366648ddf2affde1ccc20dbf23823c2210ada8
zimo493 <2081182432@qq.com>
2025-08-06 08:06:33 +0800

    feat: :sparkles: (i18n)添加加载中提示
    - 在英文、简体中文和繁体中文语言文件中添加"加载中..."提示
    
    - 更新 ContentLoading 组件以显示加载中提示
    
    - 优化 main.ts 文件结构，改进模块加载和初始化流程

--- src/components/common/ContentLoading.vue
+++ src/components/common/ContentLoading.vue
@@ -3,9 +3,11 @@
     <n-text type="info">
       <Icones :size="48" icon="line-md:loading-twotone-loop" />
     </n-text>
+    <n-text type="info">{{ $t("common.loading") }}</n-text>
   </n-flex>
 </template>
 
 <script lang="ts" setup>
 defineOptions({ name: "ContentLoading" });
+import { $t } from "@/utils";
 </script>
--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,28 @@ import { setupWebSocket } from "@/plugins/websocket";
 import AppVue from "@/App";
 import AppLoading from "@/components/common/AppLoading.vue";
 
-// 载入全局loading加载状态
+/** 载入全局loading加载状态 */
 const appLoading: App<Element> = createApp(AppLoading);
 
-const setupApp = () => {
-  // 创建vue实例
-  const app: App<Element> = createApp(AppVue);
+appLoading.mount("#appLoading");
 
-  appLoading.mount("#appLoading");
+/** 创建vue实例 */
+const app: App<Element> = createApp(AppVue);
 
-  /* 注册模块 指令/静态资源 */
-  Object.values(
-    import.meta.glob<{ install: (app: App) => void }>("./modules/*.ts", {
-      eager: true,
-    })
-  ).map((i) => app.use(i));
+/** 注册模块 指令/静态资源 */
+Object.values(
+  import.meta.glob<{ install: (app: App) => void }>("./modules/*.ts", {
+    eager: true,
+  })
+).map((i) => app.use(i));
 
-  // 注册模块Pinia
-  installPinia(app);
+/** 注册模块 Pinia */
+installPinia(app);
 
-  // 注册模块 VueRouter
-  installRouter(app)
-    .then(() => console.info("路由初始化完成"))
-    .then(() => setupWebSocket())
-    .finally(() => appLoading.unmount());
+/** 注册模块 VueRouter */
+installRouter(app)
+  .then(setupWebSocket)
+  .finally(() => appLoading.unmount());
 
-  // 挂载
-  app.mount("#app");
-};
-
-setupApp();
+/** 挂载 App */
+app.mount("#app");
