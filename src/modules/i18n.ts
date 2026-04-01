ea1e52eeed766dc94697874eff6b05cf4761247b
zimo493 <2081182432@qq.com>
2026-04-01 13:09:26 +0800

    refactor: :recycle: (i18n)重命名国际化语言文件
    - 将 i18n 模块中的导入路径从下划线格式改为连字符格式，以便 vscode 更好的显示语言

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,9 +1,9 @@
 import type { App } from "vue";
 import { local } from "@/utils";
 import { createI18n } from "vue-i18n";
-import enUS from "../../locales/en_US.json";
-import zhCN from "../../locales/zh_CN.json";
-import zhTW from "../../locales/zh_TW.json";
+import enUS from "../../locales/en-US.json";
+import zhCN from "../../locales/zh-CN.json";
+import zhTW from "../../locales/zh-TW.json";
 
 const { VITE_DEFAULT_LANG } = import.meta.env;
 
