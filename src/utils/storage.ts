3d3e8aeb6bfcb62f8941ba80970fc1d92a21cd84
zimo493 <2081182432@qq.com>
2026-04-09 11:12:26 +0800

    refactor: :recycle: (utils)重构 `storage` 工具函数
    修改 clear 方法为只清除对应前缀的存储

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,16 +1,16 @@
 const STORAGE_PREFIX = import.meta.env.VITE_STORAGE_PREFIX;
+const DEFAULT_EXPIRE = 60 * 60 * 24 * 7; // 7天，单位秒
 
 interface StorageData<T> {
   value: T;
   expire: number | null;
 }
-/**
- * LocalStorage部分操作
- */
-function createLocalStorage<T extends Local>() {
+
+// #region localStorage
+const createLocalStorage = <T extends Local>() => {
   // 默认缓存期限为7天
 
-  function set<K extends keyof T>(key: K, value: T[K], expire: number = 60 * 60 * 24 * 7) {
+  const set = <K extends keyof T>(key: K, value: T[K], expire: number = DEFAULT_EXPIRE) => {
     const storageData: StorageData<T[K]> = {
       value,
       expire: new Date().getTime() + expire * 1000,
@@ -18,9 +18,9 @@ function createLocalStorage<T extends Local>() {
     const json = JSON.stringify(storageData);
 
     window.localStorage.setItem(`${STORAGE_PREFIX}${String(key)}`, json);
-  }
+  };
 
-  function get<K extends keyof T>(key: K) {
+  const get = <K extends keyof T>(key: K) => {
     const json = window.localStorage.getItem(`${STORAGE_PREFIX}${String(key)}`);
 
     if (!json) return null;
@@ -35,34 +35,27 @@ function createLocalStorage<T extends Local>() {
     remove(key);
 
     return null;
-  }
-
-  function remove(key: keyof T) {
-    window.localStorage.removeItem(`${STORAGE_PREFIX}${String(key)}`);
-  }
-
-  function clear() {
-    window.localStorage.clear();
-  }
-
-  return {
-    set,
-    get,
-    remove,
-    clear,
   };
-}
-/**
- * sessionStorage部分操作
- */
 
-function createSessionStorage<T extends Session>() {
-  function set<K extends keyof T>(key: K, value: T[K]) {
+  const remove = (key: keyof T) => {
+    window.localStorage.removeItem(`${STORAGE_PREFIX}${String(key)}`);
+  };
+
+  const clear = () => {
+    window.localStorage.clear();
+  };
+
+  return { set, get, remove, clear };
+};
+
+// #region sessionStorage
+const createSessionStorage = <T extends Session>() => {
+  const set = <K extends keyof T>(key: K, value: T[K]) => {
     const json = JSON.stringify(value);
 
     window.sessionStorage.setItem(`${STORAGE_PREFIX}${String(key)}`, json);
-  }
-  function get<K extends keyof T>(key: K) {
+  };
+  const get = <K extends keyof T>(key: K) => {
     const json = sessionStorage.getItem(`${STORAGE_PREFIX}${String(key)}`);
 
     if (!json) return null;
@@ -72,21 +65,17 @@ function createSessionStorage<T extends Session>() {
     if (storageData) return storageData;
 
     return null;
-  }
-  function remove(key: keyof T) {
-    window.sessionStorage.removeItem(`${STORAGE_PREFIX}${String(key)}`);
-  }
-  function clear() {
-    window.sessionStorage.clear();
-  }
-
-  return {
-    set,
-    get,
-    remove,
-    clear,
   };
-}
+  const remove = (key: keyof T) => {
+    window.sessionStorage.removeItem(`${STORAGE_PREFIX}${String(key)}`);
+  };
+  const clear = () => {
+    window.sessionStorage.clear();
+  };
 
+  return { set, get, remove, clear };
+};
+
+// #region 导出
 export const local = createLocalStorage();
 export const session = createSessionStorage();
