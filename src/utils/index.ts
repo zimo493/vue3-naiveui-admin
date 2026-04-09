3580cb9f6b48eb59cecd1dc009a22f165f932ea7
zimo493 <2081182432@qq.com>
2026-04-09 11:21:58 +0800

    refactor: :recycle: (utils)重新排序导出语句

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,12 +2,12 @@ import request from "./request";
 
 export default request;
 
+export * from "./storage";
 export * from "./comm";
-export * from "./icon";
 export * from "./is";
+export * from "./i18n";
+export * from "./icon";
 export * from "./jsencrypt";
 export * from "./request";
 export * from "./routeHelper";
-export * from "./storage";
 export * from "./spin";
-export * from "./i18n";
